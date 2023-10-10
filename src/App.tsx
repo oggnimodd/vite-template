efb9992e588a0b76a7ff76e81b1d6bcfae8dd290
oggnimodd <arnolddominggo19@gmail.com>
2023-10-10 20:06:29 +0700

    upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { Button } from "components";
-import { priceAtom } from "atoms";
-import { useAtom } from "jotai";
+import { Button } from "@nextui-org/react";
+import { useCountStore } from "stores";
 
 const App = () => {
-  const [price, setPrice] = useAtom(priceAtom);
+  const count = useCountStore((state) => state.count);
+  const { increment, decrement } = useCountStore();
 
   return (
     <div className="bg-black min-h-screen w-full flex items-center justify-center flex-col gap-3">
-      <span className="text-white text-2xl font-bold">{price}</span>
+      <span className="text-white text-2xl font-bold">{count}</span>
       <div className="flex gap-x-3">
-        <Button onClick={() => setPrice((prev) => prev + 1)}> Count Up</Button>
-        <Button onClick={() => setPrice((prev) => prev - 1)}>
+        <Button color="primary" onPress={decrement}>
           {" "}
           Count Down
+        </Button>
+        <Button color="primary" onPress={increment}>
+          {" "}
+          Count Up
         </Button>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { NextUIProvider } from "@nextui-org/react";
 import "./styles/index.css";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>,
 );
