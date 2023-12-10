e051c95ccf51b0513bb8748f112fa4ead2ff13ec
dominggo1999 <arnold.ds.dev@gmail.com>
2023-12-10 15:27:45 +0000

    sync antd brekpoints with tailwind css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
 import { Counter } from "components";
-import { Button } from "antd";
+import { Button, Typography } from "antd";
 import { useDarkMode } from "hooks";
 import { Sun, Moon } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useViewportSize } from "@mantine/hooks";
+
+const CheckBreakPoints = () => {
+  const { width } = useViewportSize();
+
+  return (
+    <div>
+      <div className="w-10 h-10 bg-red-6 xs:bg-blue-6 sm:bg-green-6 md:bg-yellow-6 lg:bg-purple-6 xl:bg-magenta-6 xxl:bg-blue-6" />
+      <Typography.Text>{width}</Typography.Text>
+    </div>
+  );
+};
 
 const Home = () => {
   const { toggleTheme, isDark } = useDarkMode();
@@ -24,6 +36,9 @@ const Home = () => {
         <Link to="/about">
           <Button type="primary">Go to About</Button>
         </Link>
+      </div>
+      <div className="flex justify-center py-10">
+        <CheckBreakPoints />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ const { tailwindAntdColors } = require("./tailwind");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1600px",
+    },
     extend: {},
   },
   darkMode: "class",
