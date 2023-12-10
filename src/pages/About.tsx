cc7cd667602a246f0a053fabaa0efb54ffe3ba05
oggnimodd <arnold.ds.dev@gmail.com>
2023-12-10 15:57:27 +0700

    add mantine hooks

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,29 @@
 import { FC } from "react";
+import { Button, Modal, Typography } from "antd";
+import { useDisclosure } from "@mantine/hooks";
 
 const About: FC = () => {
+  const [isModalOpen, { open: showModal, close: closeModal }] = useDisclosure();
+
   return (
-    <div>
-      <p>Hello, World!</p>
-    </div>
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={closeModal}
+        onCancel={closeModal}
+      >
+        <Typography.Text>Content of the modal</Typography.Text>
+        <Typography.Text>Content of the modal</Typography.Text>
+        <Typography.Text>Content of the modal</Typography.Text>
+        <Typography.Text>Content of the modal</Typography.Text>
+        <Typography.Text>Content of the modal</Typography.Text>
+        <Typography.Text>Content of the modal</Typography.Text>
+      </Modal>
+    </>
   );
 };
 
